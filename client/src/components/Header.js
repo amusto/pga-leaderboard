0d4c46247fef2d20dad232276d911bec43ab5a52
Armando Musto <armando.musto@gmail.com>
2019-01-01 10:14:01 -0500

    Commit of recent changes before I branch PGA

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,18 +19,22 @@ class Header extends Component {
                 );
             default:
                 return [
-
                     <li key="0">
+                        <Link to={this.props.auth ? '/' : '/'}>
+                            Home
+                        </Link>
+                    </li>,
+                    <li key="1">
                         <Link to={this.props.auth ? '/players' : '/'}>
                             LeaderBoard
                         </Link>
                     </li>,
-                    <li key="1">
+                    <li key="2">
                         <Link to={this.props.auth ? '/userInfo' : '/'}>
-                            User Details
+                            My Profile
                         </Link>
                     </li>,
-                    <li key="2"><a href="/api/logout">Logout</a></li>
+                    <li key="3"><a href="/api/logout">Logout</a></li>
                 ]
         }
     }
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,9 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
-const mongoose = require('mongoose');
+const
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    GoogleStrategy = require('passport-google-oauth20').Strategy,
+    keys = require('../config/keys'),
+    mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
@@ -32,3 +34,18 @@ passport.use(new GoogleStrategy({
         done(null, user);
     })
 );
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.findOne({ username: username }, function(err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }
+));
